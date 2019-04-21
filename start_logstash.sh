55e3d60470880ae949f8a75f91e8eae65a65eb3d
Dan Iverson <dan@daniverson.com>
2019-04-21 08:14:53 -0500

    Add start_logstash.sh script

---
+++ start_logstash.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# shellcheck disable=2059,2154,2034,2155,2046,2086
+#===============================================================================
+# vim: softtabstop=2 shiftwidth=2 expandtab fenc=utf-8 spelllang=en ft=sh
+#===============================================================================
+#
+#          FILE: start_logstash.sh
+#
+#         USAGE: sudo ./start_logstash.sh
+#
+#   DESCRIPTION: Start Logstash from the command line
+#
+#===============================================================================
+
+set -e          # Exit immediately on error
+set -u          # Treat unset variables as an error
+set -o pipefail # Prevent errors in a pipeline from being masked
+IFS=$'\n\t'     # Set the internal field separator to a tab and newline
+
+
+###############
+#  Functions  #
+###############
+
+function echoinfo() {
+  local GC="\033[1;32m"
+  local EC="\033[0m"
+  printf "${GC} ☆  INFO${EC}: %s${GC}\n" "$@";
+}
+
+function echodebug() {
+  local BC="\033[1;34m"
+  local EC="\033[0m"
+  local GC="\033[1;32m"
+  if [[ -n ${DEBUG+x} ]]; then
+    printf "${BC} ★  DEBUG${EC}: %s${GC}\n" "$@";
+  fi
+}
+
+function echoerror() {
+  local RC="\033[1;31m"
+  local EC="\033[0m"
+  printf "${RC} ✖  ERROR${EC}: %s\n" "$@" 1>&2;
+}
+
+function start_logstash() {
+  echoinfo "Starting Logstash"
+  /usr/share/logstash/bin/logstash
+}
+
+########
+# Main #
+########
+
+start_logstash
